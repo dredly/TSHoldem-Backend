70ba0f3be1350b45ed6f8315d1a69e81df490945
Daniel Redly <dredly@gmail.com>
2023-02-14 21:12:19 +0000

    Started developing some game management functions

--- src/gameManagement.ts
+++ src/gameManagement.ts
@@ -1,9 +1,34 @@
 import { randomUUID } from "crypto";
 import { Game, Player } from "./types";
 
+export const createPlayer = (name: String): Player => {
+    return {
+        id: randomUUID(),
+        name,
+        role: "OTHER"
+    }
+}
+
 export const createGame = (player: Player): Game => {
     return {
         id: randomUUID(),
         players: [player]
+    }
+}
+
+export const joinGame = (game: Game, player: Player): Game => {
+    return {
+        ...game,
+        players: game.players.concat(player)
+    }
+}
+
+export const leaveGame = (game: Game, player: Player): Game => {
+    if (!game.players.find(p => p.id === player.id)) {
+        throw new Error("Tried to leave game but player not in it")
+    }
+    return {
+        ...game,
+        players: game.players.filter(p => p.id !== player.id)
     }
 }
--- src/test/gameManagement.test.ts
+++ src/test/gameManagement.test.ts
@@ -1,8 +1,15 @@
-import { createGame } from "../gameManagement"
-import { Player } from "../types"
+import { createGame, createPlayer, joinGame, leaveGame } from "../gameManagement"
+import { Game, Player } from "../types"
+
+describe("createPlayer function", () => {
+    it("creates a player with the correct default role", () => {
+        const player = createPlayer("Miguel")
+        expect(player.role).toBe("OTHER")
+    })
+})
 
 describe("createGame function", () => {
-    it("works", () => {
+    it("creates a game with the player who created it in the list of players", () => {
         const testPlayer: Player = {
             id: "1",
             name: "Miguel",
@@ -10,5 +17,37 @@ describe("createGame function", () => {
         }
         const newGame = createGame(testPlayer)
         expect(newGame.players).toHaveLength(1)
+        expect(newGame.players[0].id).toBe(testPlayer.id)
+    })
+})
+
+describe("joinGame function", () => {
+    it("a player can join a game", () => {
+        const host = createPlayer("Miguel")
+        const joinee = createPlayer("Bob")
+        const miguelsGame = createGame(host)
+        const miguelsGameUpdated = joinGame(miguelsGame, joinee)
+        expect(miguelsGameUpdated.players).toHaveLength(2)
+        expect(miguelsGameUpdated.players[1].name).toBe(joinee.name)
+    })
+})
+
+describe("leaveGame function", () => {
+    it("a player can leave a game", () => {
+        const players = ["bob", "alice", "jess"].map(name => createPlayer(name))
+        const game: Game = {
+            id: "1",
+            players
+        }
+        const updatedGame = leaveGame(game, players[2])
+        expect(updatedGame.players).toHaveLength(2)
+    })
+    it("throws an error if the player is not in that game already", () => {
+        const game: Game = {
+            id: "1",
+            players: [createPlayer("Miguel")]
+        }
+        const playerNotInGame = createPlayer("Outsider Joe")
+        expect(() => {leaveGame(game, playerNotInGame)}).toThrowError("Tried to leave game but player not in it")
     })
 })
